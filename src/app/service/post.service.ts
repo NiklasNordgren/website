876c09cb29613458489e2cfde2d82bedfc47d639
NiklasNordgren <39832486+NiklasNordgren@users.noreply.github.com>
2020-09-01 20:57:52 +0200

    Update post.service.ts

--- src/app/service/post.service.ts
+++ src/app/service/post.service.ts
@@ -14,8 +14,8 @@ export class PostService {
       dateString: "Tuesday, 2020-09-01",
       pictures: [],
       paragraphs: [
-        "Ok here we go, first blog post I have ever written. Let´s start off with what my intentions are for this website. First of all I´m very excited that it is up and running on a public domain. I have never done something like this before without an organization or company backing me up.",
-        "The main reason for this website is that I like creating stuff, always have. This will be the main platform I test and share stuff I create. For better or worse I easily get bored if I´m not actively doing something. So this place will be the main outlet of my boredom. My aim is to update this website with new features weekly.",
+        "Ok here we go, first blog post I have ever written. Let's start off with what my intentions are for this website. First of all I'm very excited that it is up and running on a public domain. I have never done something like this before without an organization or company backing me up.",
+        "The main reason for this website is that I like creating stuff, always have. This will be the main platform I test and share stuff I create. For better or worse I easily get bored if I'm not actively doing something. So this place will be the main outlet of my boredom. My aim is to update this website with new features weekly.",
         "",
         "Current backlog:",
         "General look and feel of the application.",
